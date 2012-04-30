70bd297eb41baec22a180eb0fb03335ebe8e30d2
dev1029 <dev1029@yandex.ru>
2012-04-30 10:28:20 +0700

    Copy items from cart to order when order is created

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,15 @@
 class Order < ActiveRecord::Base
-   has_many :positions, :dependent => :destroy
-   has_many :items, :through => :positions
+  attr_accessor :cart  
+
+  has_many :positions, :dependent => :destroy
+  has_many :items, :through => :positions
+
+  before_validation :set_item_ids
+
+  private
+    def set_item_ids
+      errors[:base] << I18n.t('cart_is_empty') if cart.items.empty?
+
+      self.item_ids = cart.item_ids
+    end
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -3,4 +3,32 @@ require 'spec_helper'
 describe Order do
   it { should have_many(:positions).dependent(:destroy) } 
   it { should have_many(:items).through(:positions) } 
+
+  describe 'items should be' do
+    let(:cart) { Fabricate :cart }
+    let(:order) { Order.new }
+
+    describe 'the same as carts items' do
+      let(:foo_item) { Fabricate :item }
+      let(:bar_item) { Fabricate :item }
+
+      before { cart.items << foo_item }
+      before { cart.items << bar_item }
+
+      before { order.cart = cart }
+      before { order.save }
+
+      specify { order.should be_persisted }
+      specify { order.items.should == [foo_item, bar_item] }
+    end
+
+    describe 'be empty if cart is empty' do
+      before { order.cart = cart }
+      before { order.save }
+
+      specify { order.should_not be_persisted }
+      specify { order.should_not be_valid }
+      specify { order.errors.messages[:base].should == [I18n.t('cart_is_empty')] }
+    end
+  end
 end
