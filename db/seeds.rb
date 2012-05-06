28a029e2923c4021521e0ff716118073b47d857b
dev1029 <dev1029@yandex.ru>
2012-05-06 10:04:42 +0700

    More real seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,15 @@
-Category.destroy_all
+# encoding: utf-8
 
 Category.root.tap do |catalog|
-  catalog.children.create(:title => 'Computers').tap do |computers|
-
-    computers.children.create(:title => 'Notebooks').tap do |notebooks|
-      notebooks.items.create(:vendor => 'HP', :model => 'PAVILION dv6-6c51er').tap do |notebook|
-        ['Processor Core i5', 'RAM 4Gb', 'HDD 500 Gb'].each do |text|
-          notebook.properties.create :text => text
-        end
-
-        notebook.images.create! :file => File.open("#{Rails.root}/spec/fixtures/hp.jpg")
-      end
+  catalog.children.find_or_create_by_title('Ноутбуки').tap do |notebooks|
+    %w[Acer Apple ASUS DELL Toshiba HP Lenovo MSI Samsung Sony].each do |vendor|
+      notebooks.children.find_or_create_by_title("Ноутбуки #{vendor}") 
     end
+  end
 
-    computers.children.create(:title => 'Netbooks').tap do |netbooks|
-      netbooks.items.create(:vendor => 'Asus', :model => 'Eee PC 1225B').tap do |netbook|
-        ['Processor E-450', 'RAM 2Gb', 'HDD 320 Gb'].each do |text|
-          netbook.properties.create :text => text
-        end
-
-        netbook.images.create! :file => File.open("#{Rails.root}/spec/fixtures/asus.jpg")
-      end
+  catalog.children.find_or_create_by_title('Мониторы').tap do |monitors|
+    %w[Acer AOC ASUS BenQ E-Mashines LG Phillips Samsung ViewSonic].each do |vendor|
+      monitors.children.find_or_create_by_title("Moниторы #{vendor}") 
     end
   end
 end
